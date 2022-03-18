f1ee359d1ddfe46c807cf192d4621e5589cec63f
Sam331 <36762547+zyhusesit@users.noreply.github.com>
2022-03-18 22:48:43 +0800

    All done!

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
-#include <string>
+#include <valarray>
 
 using namespace std;
 
-typedef struct MyStack {
-    int *s;//用动态数组表示栈，栈的深度等于盘子的数量
+struct MyStack {
+    int *stack;//用动态数组表示栈，栈的深度等于盘子的数量
     char name;//栈的名称
     int top;//栈顶位置
     int maxsize;//栈的最大容量，等于盘子的数量
-} ST;//定义栈的结构
+};//定义栈的结构
 
-void init(ST *T, int n);//初始化栈（为表示栈的数组分配空间，给栈的各项参数赋初始值）
-void destroy(ST *T);//销毁栈（释放表示栈的数组的空间）
-int isFull(ST *t);//判断栈是否满，满返回1，否则返回0
-int isEmpty(ST *t);//判断栈是否空，空返回1，否则返回0
-void push(ST *t, int index);//向栈顶放入一个编号为index的盘子
-int pop(ST *t);//弹出栈顶的盘子，返回盘子序号
-void hanoi(ST *t, int n);//模拟实现n个盘子的汉诺塔问题
-void showStack(ST *T);//显示所有栈中的内容
-int getTop(ST *t);//返回栈顶盘子的编号
+void initializeStack(MyStack *T, int n);//初始化栈（为表示栈的数组分配空间，给栈的各项参数赋初始值）
+void destroy(MyStack *T);//销毁栈（释放表示栈的数组的空间）
+bool isFull(MyStack *t);//判断栈是否满，满返回1，否则返回0
+bool isEmpty(MyStack *t);//判断栈是否空，空返回1，否则返回0
+void push(MyStack *t, int index);//向栈顶放入一个编号为index的盘子
+int pop(MyStack *t);//弹出栈顶的盘子，返回盘子序号
+void hanoi(MyStack *t, int n);//模拟实现n个盘子的汉诺塔问题
+void showStack(MyStack *T);//显示所有栈中的内容
+int getTop(MyStack *t);//返回栈顶盘子的编号
+void move(MyStack *source, MyStack *target);
+
+void moveHelper(MyStack *stack1, MyStack *stack2);
+
+void hanoiRecursion(MyStack *source, MyStack *target, MyStack *auxiliary, int n);
 
 int main() {
-    ST T[3];//T[0],T[1],T[2]分别表示3个栈
+    MyStack T[3];//T[0],T[1],T[2]分别表示3个栈
     int n;//盘子的数量
+
     cout << "请输入盘子的数量:" << endl;
+    cin >> n;
 
     while (n > 0 && n < 64) {
-        init(T, n);//对三个栈做初始化
-        cout << "the init stack is:" << endl;
+        initializeStack(T, n);//对三个栈做初始化
+        cout << "the initializeStack stack is:" << endl;
         showStack(T);//显示初始栈的情况（所有盘子都在A，B与C都是空的）
         hanoi(T, n);//模拟实现汉诺塔操作
         cout << "after move, the stack is now:" << endl;
@@ -36,97 +43,138 @@ int main() {
         cout << "请继续输入盘子的数量:" << endl;
         cin >> n;
     }
+    return 0;
 }
 
-void init(ST *T, int n)//对三个栈做初始化处理，A是源地址，B是中转，C是目的地
+void initializeStack(MyStack *T, int n)//对三个栈做初始化处理，A是源地址，B是中转，C是目的地
 {
     int i;
+
     for (i = 0; i < 3; i++) {
-        T[i].s = new int[n];//为每个栈的数组分配空间,栈的深度等于盘子数量
+        T[i].stack = new int[n];//为每个栈的数组分配空间,栈的深度等于盘子数量
         T[i].maxsize = n;//栈的最大容量
     }
+
     T[0].name = 'A';//第一个栈的名字是A
+    T[1].name = 'B';
+    T[2].name = 'C';
+
     for (i = 0; i < n; i++)//初始情况下所有的盘子都在A，编号越大盘子越大，最小号盘子在栈顶
-        T[0].s[i] = n - i;
+        T[0].stack[i] = n - i;
+
     T[0].top = n - 1;//初始情况下A是满的
     T[1].top = T[2].top = -1;//除了A，剩余两个栈都是空的
-    if (n % 2 == 0)//根据盘子总数的奇偶数，给剩余的两个栈命名
-    {
-        T[1].name = 'B';
-        T[2].name = 'C';
-    } else {
-        T[1].name = 'C';
-        T[2].name = 'B';
-    }
 }
 
-void destroy(ST *T) {
+void destroy(MyStack *T) {
     int i;
     for (i = 0; i < 3; i++)
-        delete T[i].s;
+        delete T[i].stack;
 }
 
-
-int isFull(ST *t) {
+bool isFull(MyStack *t) {
     if (t->top == t->maxsize - 1)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
-int isEmpty(ST *t) {
+bool isEmpty(MyStack *t) {
     if (t->top < 0)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
 
-void push(ST *t, int index) {
-    if (isFull(t)) {
+void push(MyStack *T, int index) {
+    if (isFull(T)) {
         cout << "the stack is full ,can not push, this step is wrong" << endl;
         exit(0);
     }
-    (t->top)++;
-    t->s[t->top] = index;
+    (T->top)++;
+    T->stack[T->top] = index;
 }
 
-int pop(ST *t) {
+int pop(MyStack *t) {
     int x;
     if (isEmpty(t)) {
         cout << "the stack is empty ,can not pop, this step is wrong" << endl;
         exit(0);
     }
-    x = t->s[t->top];
+    x = t->stack[t->top];
     (t->top)--;
     return x;
 }
 
-int getTop(ST *t) {
+int getTop(MyStack *t) {
     if (isEmpty(t)) {
         cout << "the stack is empty ,can not get the top, this step is wrong" << endl;
         exit(0);
     }
-    return (t->s[t->top]);
+    return (t->stack[t->top]);
 }
 
-
-void showStack(ST *T)//移动结束后，显示三个栈的内容
+void showStack(MyStack *T)//移动结束后，显示三个栈的内容
 {
     int i, j;
+    cout << "###########################################################" << endl;
+
     for (i = 0; i < 3; i++) {
         if (T[i].top >= 0) {
-            cout << "the" << T[i].name << "stace has the plate:" << endl;
+            cout << "the " << T[i].name << " stace has the plate:" << endl;
             for (j = T[i].top; j >= 0; j--)
-                cout << T[i].s[j] << endl;
+                cout << T[i].stack[j] << endl;
         } else
-            cout << "the" << T[i].name << "stack is empty" << endl;
+            cout << "the " << T[i].name << " stack is empty" << endl;
     }
-    printf("\n");
+    cout << "###########################################################" << endl;
 }
 
-
-void hanoi(ST *T, int n)//处理过程中把盘子移动的过程记录下来，就是要输出“将XX号盘子从XX移动到XX”
+void hanoi(MyStack *T, int n)//处理过程中把盘子移动的过程记录下来，就是要输出“将XX号盘子从XX移动到XX”
 {
-
+    int total_movement = 1;
+    while (n--) {
+        total_movement *= 2;
+    }
+    for (int i = 1; i < total_movement; ++i) {
+        if (i % 3 == 0)
+            moveHelper(T + 1, T + 2);
+        else if (i % 3 == 1)
+            moveHelper(T, T + 2);
+        else if (i % 3 == 2)
+            moveHelper(T, T + 1);
+    }
 }
+
+void move(MyStack *source, MyStack *target) {
+    if (isEmpty(source))
+        cout << "Error! The source stack is empty." << endl;
+    if (isFull(target))
+        cout << "Error! The destination stack is full." << endl;
+
+    cout << "Move the disc " << getTop(source)
+         << " from " << source->name << " to " << target->name << endl;
+    push(target, pop(source));
+}
+
+void moveHelper(MyStack *stack1, MyStack *stack2) {
+    if (isEmpty(stack1))
+        move(stack2, stack1);
+    else if (isEmpty(stack2))
+        move(stack1, stack2);
+    else if (getTop(stack1) > getTop(stack2))
+        move(stack2, stack1);
+    else
+        move(stack1, stack2);
+}
+
+//void hanoiRecursion(MyStack *source, MyStack *target, MyStack *auxiliary, int n) {
+//    if (n > 0) {
+//        hanoiRecursion(source, auxiliary, target, n - 1);
+//        move(source, target);
+//        hanoiRecursion(auxiliary, target, source, n - 1);
+//    }
+//}
+
+
 
